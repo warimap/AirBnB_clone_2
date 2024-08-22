051f6926dc6d9828b44f909276872eddeb2d8c67
Faith Warima Ng'endo <warimapharium@gmail.com>
2024-08-22 14:19:19 +0300

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
+env.hosts = ['34.204.60.71', '34.207.155.190']
 
 
 def do_clean(number=0):
