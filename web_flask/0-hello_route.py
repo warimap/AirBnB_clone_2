45d75a15d95d80786addb0a4934b82d732f5bc7d
Faith Warima Ng'endo <warimapharium@gmail.com>
2024-08-22 16:22:31 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/airbnb-onepage/')
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
+    """Return a string at the /airbnb-onepage/ route"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
